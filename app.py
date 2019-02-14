137d851876469018b374bdb2880b86bd694f06d7
Harry Sibbenga <sibbengaharry@gmail.com>
2019-02-14 18:35:46 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'hello world'
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
