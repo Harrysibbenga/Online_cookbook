9c81db308751dfd63b996e3dcb2a5e1e8882c3fa
Harry Sibbenga <sibbengaharry@gmail.com>
2019-02-18 23:16:50 +0000

    Created the recipes.html page to test that the application diaplays information from mongodb atlas database, a black base.html page is present that will be edited later, requirements.txt file updated

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = 'online_cookbook'
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'hello world'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template('recipes.html', recipes=mongo.db.recipes.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
