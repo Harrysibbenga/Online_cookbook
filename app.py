d959e969915082bb5cb373065ef37bd8024a78a8
Harry Sibbenga <sibbengaharry@gmail.com>
2019-02-24 12:12:35 +0000

    Created style.css file, added some jquery to initialise the form select, created the layout for the recipes.html page and linked the select form to the database to render instances of the records in the database and display them.

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template('recipes.html', recipes=mongo.db.recipes.find())
+    return render_template('recipes.html', recipes=mongo.db.recipes.find(), 
+    authors=mongo.db.authors.find(), allergins=mongo.db.allergins.find(), types=mongo.db.types.find(),
+    countries=mongo.db.countries.find(), cuisines=mongo.db.cuisines.find(), diets=mongo.db.diets.find(),
+    origins=mongo.db.origins.find(), categories=mongo.db.categories.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
